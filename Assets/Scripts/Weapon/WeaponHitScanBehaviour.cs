38cdbf0a526ae16ff7d701383aac02a806657dd4
Bryon <bryon.guidry1@gmail.com>
2024-01-11 15:20:03 -0600

    Auto stash before merge of "WeaponFiring" and "main"

---
+++ Assets/Scripts/Weapon/WeaponHitScanBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHitScanBehaviour : MonoBehaviour
+{
+
+    [SerializeField, Tooltip("Layers that the bullets can collide with.")]
+    private LayerMask _layerMask;
+    private AudioClip _fireSound;
+    private AudioClip _hitSound; 
+
+    private
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
