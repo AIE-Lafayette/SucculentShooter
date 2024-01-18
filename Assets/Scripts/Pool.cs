684f09efa0d8f4d451b31494f424aab410c70245
Ryan Powell <s228032@students.aie.edu>
2024-01-18 17:05:54 -0600

    Mostly finished ObjectPoolBehaviour, still needs tweaking. Unable to SetActive(false) properly

--- Assets/Scripts/Pool.cs
+++ Assets/Scripts/Pool.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class Pool
 {
+    /// <summary>
+    /// The name of the game object that this pool will store.
+    /// </summary>
     private string _name;
     private List<GameObject> _objects;
 
     public string Name;
     public int Count { get { return _objects.Count; } }
 
-    Pool(GameObject gameObject)
+    /// <summary>
+    /// Initializes the list and sets the name to be the game object's name.
+    /// </summary>
+    /// <param name="gameObject"></param>
+    public Pool(GameObject gameObject)
     {
         _objects = new List<GameObject>();
         _name = gameObject.name;
 
     }
 
+    /// <summary>
+    /// Returns whether or not this object instance is in the pool.
+    /// </summary>
+    /// <param name="objectInstance"></param>
+    /// <returns></returns>
     public bool Contains(GameObject objectInstance)
     {
         return _objects.Contains(objectInstance);
     }
 
+    /// <summary>
+    /// Finds the last object added to the pool and removes it from the pool.
+    /// </summary>
+    /// <returns></returns>
     public GameObject GetObject()
     {
         GameObject obj = _objects[_objects.Count];
@@ -30,6 +46,10 @@ public class Pool
         return obj;
     }
 
+    /// <summary>
+    /// Adds an item back into the pool.
+    /// </summary>
+    /// <param name="gameObject"></param>
     public void ReturnObject(GameObject gameObject)
     {
         _objects.Add(gameObject);
