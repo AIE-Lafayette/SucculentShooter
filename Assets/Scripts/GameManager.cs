4f4e7a57e2e89ac8b94b8e6c0a6255080b4dc0af
Bryon <bryon.guidry1@gmail.com>
2024-01-23 14:59:44 -0600

    Sound, gamemanager updates, and input behaviour

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,55 +7,45 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager _instance;
-    public static GameManager Instance
-    {
-        get
-        {
-            if (!_instance)
-                _instance = FindObjectOfType<GameManager>();
+	private static GameManager _instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (!_instance)
+				_instance = FindObjectOfType<GameManager>();
 
-            if (!_instance)
-            {
-                Debug.Log("No GameManager found in scene. Creating one.");
-                _instance = new GameObject("GameManager").AddComponent<GameManager>();
-            }
+			if (!_instance)
+			{
+				Debug.Log("No GameManager found in scene. Creating one.");
+				_instance = new GameObject("GameManager").AddComponent<GameManager>();
+			}
 
-            return _instance;
-        }
-    }
+			return _instance;
+		}
+	}
 
-    [Tooltip("Player object instance.")]
-    public GameObject Player;
+	[Tooltip("Player object instance.")]
+	public GameObject Player;
 
-    [Tooltip("The current score for the game.")]
-    public int Score = 0;
+	[Tooltip("The current score for the game.")]
+	public int Score = 0;
 
-    private void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+	private void RestartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
 
-    private void OnDeath()
-    {
-        // end menu with score
+	private void Start()
+	{
+		if (Player != null)
+		{
+			HealthBehaviour healthBehaviour = Player.GetComponent<HealthBehaviour>();
 
-
-        RestartGame();
-    }
-
-    private void Start()
-    {
-        if (Player != null)
-        {
-            HealthBehaviour healthBehaviour = Player.GetComponent<HealthBehaviour>();
-
-            if (healthBehaviour == null)
-                healthBehaviour = Player.AddComponent<HealthBehaviour>();
-            
-
-
-            healthBehaviour.AddOnDeathAction(OnDeath);
-        }
-    }
+			if (healthBehaviour == null)
+				healthBehaviour = Player.AddComponent<HealthBehaviour>();
+			
+			healthBehaviour.AddOnDeathAction(RestartGame);
+		}
+	}
 }
